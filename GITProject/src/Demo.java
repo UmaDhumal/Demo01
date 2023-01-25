c606ef6d271347f735ddee46ee7e50431f61f184
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-25 21:15:17 +0530

    by Shobha

--- GITProject/src/Demo.java
+++ GITProject/src/Demo.java
@@ -5,6 +5,10 @@ public class Demo
 	{
 		System.out.println("M1 Method by Om");
 	}
+	public void m2()
+	{
+		System.out.println("M2 Method by Shobha");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World");
