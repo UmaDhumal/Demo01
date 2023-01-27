90321c385c727596ab8e410511bc103fa7f1a504
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-27 23:38:33 +0530

    by Anil

--- umaGIT/src/com/day01/A.java
+++ umaGIT/src/com/day01/A.java
@@ -2,6 +2,10 @@ package com.day01;
 
 public class A 
 {
+	public void m1()
+	{
+		System.out.println("m1 Method by Anil");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("A class");
